bbf05152b129ed4812d7de8d903c93bb2ae0957a
XaLVaUA <31803262+XaLVaUA@users.noreply.github.com>
2019-03-17 17:43:23 +0200

    Delete IApartmentView.java

--- Labs/Lab1/src/IApartmentView.java
+++
@@ -1,6 +0,0 @@
-public interface IApartmentView {
-    public void showApartment(Apartment apartment);
-    public void showApartments(Apartment[] apartments);
-    public void showMessage(String message);
-    public void showErrorMessage(String message);
-}
