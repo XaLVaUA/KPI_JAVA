321dea40582a375850e8af44dd7228f81addfdde
XaLVaUA <31803262+XaLVaUA@users.noreply.github.com>
2019-03-17 17:43:33 +0200

    Delete IApartmentsModel.java

--- Labs/Lab1/src/IApartmentsModel.java
+++
@@ -1,8 +0,0 @@
-public interface IApartmentsModel {
-    public void recreateAppartments(int len);
-    public void addApartment(Apartment apartment);
-    public int getLastApartmentIndex();
-    public Apartment[] getApartments();
-    public Apartment[] getApartments(int roomNumber);
-    public Apartment[] getApartments(double moreThanSquare, int higherThanFloor);
-}
